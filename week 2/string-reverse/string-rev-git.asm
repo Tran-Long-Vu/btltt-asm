60858b88cb48ec384068662edb0b4c644d26ea58
Longkakaa <tranlongvu007@gmail.com>
2022-10-18 11:51:30 +0700

    week 2 start again

--- week 2/string-reverse/string-rev-git.asm
+++ week 2/string-reverse/string-rev-git.asm
@@ -1,61 +1,73 @@
-; ------------------------
-; reverse.asm - Reverse a string without copying it
-; Max length of string is 64 bytes
-; This is for an x86_64 Linux System
-; Assemble with:
-;     yasm -f elf64 reverse.asm
-; Link with:
-;     ld -o reverse reverse.o
-; Note: Also perfectly capable of swapping yasm for nasm without changing code
-; ------------------------
-[bits 64]
 
-section .data
-msg times 64 db 0 ; distenict byte 64times as zero 0
-max equ 64
+section .bss
+    string  resb  256
 
 section .text
-global main
+    global _start
+_start:
+    mov     rbp, rsp
+    ; get user input
+    mov     rdi, 0
+    mov     rsi, string
+    mov     rdx, 256
+    mov     rax, 0
+    syscall
+    
+    mov     rdi, string
+    call    reverse
+    
+    mov     rsi, rax
+    mov     rdi, 1
+    call    println
+    
+    mov     rdi, 0
+    mov     rax, 60
+    syscall
 
-main:
-; Read
-mov rdx,max
-mov rsi,msg
-mov rdi,0
-mov rax,0
-syscall
 
-mov rcx,rax     ; Copy the string for later
-mov rdi,msg     ; Set RDI and RSI to point at message
-mov rsi,msg     ; proc: set RDI at last
-                ; RSI at fist letter
 
-add rdi,rax     ; RDI should point at last character in message
-dec rdi         ;
-shr rax,1       ; Divide length by 2
 
-.loop           ; Begin loop:
-mov bl,[rsi]    ; Swap the characters using 8 bit registers
-mov dl,[rdi]    ; mov first char to rbx
-mov [rsi],dl    ; mov last char to rdx
-mov [rdi],bl   ; mov last char in rsi
-                ; mov first char in rdi
-                ; swapped.
 
-inc rsi         ; Increment rsi (which is a pointer)
-dec rdi         ; Decrement rdi (also a pointer)
-dec rax         ; Decrement our counter of chars. loop 1/2time the length
-jnz .loop       ; If our counter isn't zero, keep looping
 
-; Write
-.write
-mov rdx,rcx ; nbytes
-mov rsi,msg ; *msg
-mov rdi,1 ; stream
-mov rax,1 ; syscall
-syscall
+println:    ; print with linefeed by append linefeed to string
+    mov     al, 0xa
+    mov     [rsi + rdx], al
+    inc     rdx
+    mov     rax, 1
+    syscall
+    ret
 
-; Exit
-mov rdi,0 ; exit code
-mov rax,60 ; syscall
-syscall
+
+
+reverse:    ; reverse(str) use stack to store each byte and pop to reverse the string str, return *st in rax, str.size in rdx
+    push    rbp
+    mov     rbp, rsp  
+    mov     rsi, rdi
+    xor     ax, ax
+    xor     rcx, rcx
+    cld                     ; clear direction flag DF
+    
+    .iterPush:              ; iterate string and push to stack
+    lodsb                   ; al = byte ptr [esi]++
+    cmp     al, 0xa
+    jng     .pop
+    push    ax              ; push 16bit
+    inc     cx
+    jmp     .iterPush
+
+    .pop:
+    mov     rdx, rcx
+    mov     rsi, rdi        ; save begin of str
+    .iterPop:               ; pop back
+    pop     ax
+    stosb                   ; byte ptr [rdi]-- = al
+    dec     cx
+    test    cx, cx
+    jz      .done
+    jmp     .iterPop
+    
+    .done:
+    mov     rax, rsi
+    mov     rsp, rbp
+    pop     rbp
+    ret  
